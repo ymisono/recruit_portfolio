73277d5c3b7e6e21ad13cc03e2ff87bde03ea8bd
doncot <doncot@gmail.com>
2015-05-19 17:41:39 +0900

    [add] 登録処理

---
+++ ClientTest/Models/Authorizer.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Livet;
+using System.Security;
+using System.Net.Http;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Json;
+using System.IO;
+using System.Net.Http.Headers;
+using System.Windows;
+
+namespace ClientTest.Models
+{
+    /// <summary>
+    /// 認証に必要なモデル
+    /// </summary>
+    [DataContract]
+    public class Authorizer : NotificationObject
+    {
+        [DataMember(Name = "Email")]
+        public String UserName { get; set; }
+
+        [DataMember(Name = "Password")]
+        public String Password { get; set; }
+
+        [DataMember(Name = "ConfirmPassword")]
+        public String ConfirmPassword { get; set; }
+
+        public async void Register()
+        {
+            using (var client = new HttpClient())
+            {
+
+                var serializer = new DataContractJsonSerializer(typeof(Authorizer));
+                using (var ms = new MemoryStream())
+                {
+                    serializer.WriteObject(ms, this);
+                    var tempJson = Encoding.UTF8.GetString(ms.ToArray());
+                    var content = new StringContent(tempJson,Encoding.UTF8,"application/json");
+
+                    client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json")); 
+                    var url = new Uri("http://oneserversite.azurewebsites.net/api/Account/Register");
+                    var res = await client.PostAsync(url, content);
+                    if (res.IsSuccessStatusCode == true)
+                    {
+                        MessageBox.Show(String.Format("登録しました！\nようこそ{0}さん",UserName));
+                    }
+                    else
+                    {
+                        MessageBox.Show("登録できませんでした。");
+                    }
+                }
+            }
+        }
+        
+        public async void Login()
+        {
+
+        }
+    }
+}
---
+++ ClientTest/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.ComponentModel;
+
+using Livet;
+using Livet.Commands;
+using Livet.Messaging;
+using Livet.Messaging.IO;
+using Livet.EventListeners;
+using Livet.Messaging.Windows;
+
+using ClientTest.Models;
+using System.Security;
+
+namespace ClientTest.ViewModels
+{
+    public class MainWindowViewModel : ViewModel
+    {
+        /* コマンド、プロパティの定義にはそれぞれ 
+         * 
+         *  lvcom   : ViewModelCommand
+         *  lvcomn  : ViewModelCommand(CanExecute無)
+         *  llcom   : ListenerCommand(パラメータ有のコマンド)
+         *  llcomn  : ListenerCommand(パラメータ有のコマンド・CanExecute無)
+         *  lprop   : 変更通知プロパティ(.NET4.5ではlpropn)
+         *  
+         * を使用してください。
+         * 
+         * Modelが十分にリッチであるならコマンドにこだわる必要はありません。
+         * View側のコードビハインドを使用しないMVVMパターンの実装を行う場合でも、ViewModelにメソッドを定義し、
+         * LivetCallMethodActionなどから直接メソッドを呼び出してください。
+         * 
+         * ViewModelのコマンドを呼び出せるLivetのすべてのビヘイビア・トリガー・アクションは
+         * 同様に直接ViewModelのメソッドを呼び出し可能です。
+         */
+
+        /* ViewModelからViewを操作したい場合は、View側のコードビハインド無で処理を行いたい場合は
+         * Messengerプロパティからメッセージ(各種InteractionMessage)を発信する事を検討してください。
+         */
+
+        /* Modelからの変更通知などの各種イベントを受け取る場合は、PropertyChangedEventListenerや
+         * CollectionChangedEventListenerを使うと便利です。各種ListenerはViewModelに定義されている
+         * CompositeDisposableプロパティ(LivetCompositeDisposable型)に格納しておく事でイベント解放を容易に行えます。
+         * 
+         * ReactiveExtensionsなどを併用する場合は、ReactiveExtensionsのCompositeDisposableを
+         * ViewModelのCompositeDisposableプロパティに格納しておくのを推奨します。
+         * 
+         * LivetのWindowテンプレートではViewのウィンドウが閉じる際にDataContextDisposeActionが動作するようになっており、
+         * ViewModelのDisposeが呼ばれCompositeDisposableプロパティに格納されたすべてのIDisposable型のインスタンスが解放されます。
+         * 
+         * ViewModelを使いまわしたい時などは、ViewからDataContextDisposeActionを取り除くか、発動のタイミングをずらす事で対応可能です。
+         */
+
+        /* UIDispatcherを操作する場合は、DispatcherHelperのメソッドを操作してください。
+         * UIDispatcher自体はApp.xaml.csでインスタンスを確保してあります。
+         * 
+         * LivetのViewModelではプロパティ変更通知(RaisePropertyChanged)やDispatcherCollectionを使ったコレクション変更通知は
+         * 自動的にUIDispatcher上での通知に変換されます。変更通知に際してUIDispatcherを操作する必要はありません。
+         */
+
+        private Authorizer _authorizer;
+
+
+        #region DisplayUserName変更通知プロパティ
+        private string _DisplayUserName;
+
+        public string DisplayUserName
+        {
+            get
+            { return _DisplayUserName; }
+            set
+            { 
+                if (_DisplayUserName == value)
+                    return;
+                _DisplayUserName = value;
+                RaisePropertyChanged();
+            }
+        }
+        #endregion
+
+
+        #region DisplayPassword変更通知プロパティ
+        private String _DisplayPassword;
+
+        public String DisplayPassword
+        {
+            get
+            { return _DisplayPassword; }
+            set
+            { 
+                if (_DisplayPassword == value)
+                    return;
+                _DisplayPassword = value;
+                RaisePropertyChanged();
+            }
+        }
+        #endregion
+
+
+        #region RegisterCommand
+        private ViewModelCommand _RegisterCommand;
+
+        public ViewModelCommand RegisterCommand
+        {
+            get
+            {
+                if (_RegisterCommand == null)
+                {
+                    _RegisterCommand = new ViewModelCommand(Register, CanRegister);
+                }
+                return _RegisterCommand;
+            }
+        }
+
+        public bool CanRegister()
+        {
+            //if ( !String.IsNullOrWhiteSpace(DisplayUserName) 
+            //    && !String.IsNullOrWhiteSpace(DisplayPassword.ToString()) )
+            //    return true;
+            //else return false;
+            return true;
+        }
+
+        public void Register()
+        {
+            if ( !String.IsNullOrWhiteSpace(DisplayUserName)
+                && !String.IsNullOrWhiteSpace(DisplayPassword) )
+            {
+                _authorizer.UserName = DisplayUserName;
+                _authorizer.Password = DisplayPassword.ToString();
+                _authorizer.ConfirmPassword = DisplayPassword.ToString();
+                _authorizer.Register();
+            }
+        }
+        #endregion
+
+
+        public void Initialize()
+        {
+            _authorizer = new Authorizer();
+        }
+    }
+}
