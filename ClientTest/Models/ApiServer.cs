141bbe48211e517c4a473ecd476fd29622d16eea
misono <misono@sos.info.hiroshima-cu.ac.jp>
2015-06-10 03:11:23 +0900

    わからねぇ

--- ClientTest/Models/ApiServer.cs
+++ ClientTest/Models/ApiServer.cs
@@ -35,39 +35,39 @@ namespace ClientTest.Models
         /// 汎用的な取得メソッド
         /// </summary>
         /// <typeparam name="T">データを詰めたいDTOを指定。</typeparam>
-        public async Task<T> Fetch<T>(String controller)
-        {
-            //ログインしているか確認
-            if (!MySession.IsLoggedIn) return default(T);
+        //public async Task<T> Fetch<T>(String controller)
+        //{
+        //    //ログインしているか確認
+        //    if (!MySession.IsLoggedIn) return default(T);
 
-            ////接続開始
-            //using (var client = new HttpClient())
-            //{
-            //    //認証の為に、トークンをヘッダーにセット。
-            //    client.DefaultRequestHeaders.Authorization =
-            //        new AuthenticationHeaderValue("Bearer", MySession.AccessToken);
+        //    //接続開始
+        //    using (var client = new HttpClient())
+        //    {
+        //        //認証の為に、トークンをヘッダーにセット。
+        //        client.DefaultRequestHeaders.Authorization =
+        //            new AuthenticationHeaderValue("Bearer", MySession.AccessToken);
 
-            //    var res = await client.GetAsync(String.Format(
-            //            "{0}{1}", _apiPath, controller)
-            //            );
+        //        var res = await client.GetAsync(String.Format(
+        //                "{0}{1}", _apiPath, controller)
+        //                );
 
-            //    MySession.CheckResponseStatus(res);
+        //        MySession.CheckResponseStatus(res);
 
-            //    //中身を展開
-            //    var content = await res.Content.ReadAsStringAsync();
+        //        //中身を展開
+        //        var content = await res.Content.ReadAsStringAsync();
 
-            //    //中身をチェック
-            //    if (!String.IsNullOrEmpty(content) && content != "null")
-            //    {
-            //        //辞書型の列挙に詰める
-            //        var dics = JsonConvert.DeserializeObject<Dictionary<string, T>>(content);
-                    
-            //        foreach(var dic in dics)
-            //        {
-                        
-            //        }
-            //     }
-            //}
-        }
+        //        //中身をチェック
+        //        if (!String.IsNullOrEmpty(content) && content != "null")
+        //        {
+        //            //辞書型の列挙に詰める
+        //            var dics = JsonConvert.DeserializeObject<Dictionary<string, T>>(content);
+
+        //            foreach(var dic in dics)
+        //            {
+
+        //            }
+        //         }
+        //    }
+        //}
     }
 }
