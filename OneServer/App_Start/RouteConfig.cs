199f969f1030a9320b877560310d655f16e66138
doncot <doncot@gmail.com>
2015-06-11 19:47:32 +0900

    route設定でホームを抹消

--- OneServer/App_Start/RouteConfig.cs
+++ OneServer/App_Start/RouteConfig.cs
@@ -16,7 +16,8 @@ namespace OneServer
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                //defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { id = UrlParameter.Optional }
             );
         }
     }
