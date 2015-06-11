1c0a1ad7555d00b889f956763449aa36da632ae8
misono <misono@sos.info.hiroshima-cu.ac.jp>
2015-06-11 10:38:20 +0900

    単体テストプロジェクトの

---
+++ UnitTester/ApiServerTest.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ClientTest.Models;
+using System.Threading.Tasks;
+
+namespace UnitTester
+{
+    [TestClass]
+    public class ApiServerTest
+    {
+        [TestMethod]
+        public async Task LoginTest()
+        {
+            ApiServer server = new ApiServer();
+            await server.CurrentSession.LoginAsync("misono","password");
+
+            //ログイン成功しているはず
+            Assert.IsTrue(server.CurrentSession.IsLoggedIn);
+
+            //パスワードが違う
+            try
+            {
+                await server.CurrentSession.LoginAsync("misono", "まちがい");
+                //ここに来ることはない
+                Assert.Fail();
+            }
+            catch(ApplicationException)
+            {
+                //ここに来てほしい
+            }
+
+            //存在しないユーザー
+            try
+            {
+                await server.CurrentSession.LoginAsync("誰ですかこれ？", "まちがい");
+                Assert.Fail();
+            }
+            catch (ApplicationException){}
+        }
+    }
+}
