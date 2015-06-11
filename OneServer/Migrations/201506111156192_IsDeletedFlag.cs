728795396c961854aeda27d2c1027f703c1ee728
doncot <doncot@gmail.com>
2015-06-11 21:20:00 +0900

    is delted migration

---
+++ OneServer/Migrations/201506111156192_IsDeletedFlag.cs
@@ -0,0 +1,18 @@
+namespace OneServer.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class IsDeletedFlag : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.AspNetUsers", "IsDeleted", c => c.Boolean(nullable: false));
+        }
+        
+        public override void Down()
+        {
+            DropColumn("dbo.AspNetUsers", "IsDeleted");
+        }
+    }
+}
