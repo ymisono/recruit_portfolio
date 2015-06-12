5115b3a7fe6652672112edeecedef8ccaa907874
misono <misono@sos.info.hiroshima-cu.ac.jp>
2015-06-12 10:57:28 +0900

    ログインのようこそアニメーション

--- ClientTest/ViewModels/LoginViewModel.cs
+++ ClientTest/ViewModels/LoginViewModel.cs
@@ -13,6 +13,8 @@ using Livet.Messaging.Windows;
 
 using ClientTest.Models;
 using System.Windows;
+using System.Windows.Threading;
+using System.Windows.Media.Animation;
 
 namespace ClientTest.ViewModels
 {
@@ -46,6 +48,70 @@ namespace ClientTest.ViewModels
             }
         }
 
+        private String _notifyMessage;
+        public String NotifyMessage
+        {
+            get { return _notifyMessage; }
+            set
+            {
+                _notifyMessage = value;
+                RaisePropertyChanged();
+            }
+        }
+
+
+        #region IsMenuHidden変更通知プロパティ
+        private bool _IsMenuHidden;
+
+        public bool IsMenuHidden
+        {
+            get
+            { return _IsMenuHidden; }
+            set
+            { 
+                if (_IsMenuHidden == value)
+                    return;
+                _IsMenuHidden = value;
+                RaisePropertyChanged();
+            }
+        }
+        #endregion
+
+
+        #region LoadMessage変更通知プロパティ
+        private string _LoadMessage;
+
+        public string LoadMessage
+        {
+            get
+            { return _LoadMessage; }
+            set
+            { 
+                if (_LoadMessage == value)
+                    return;
+                _LoadMessage = value;
+                RaisePropertyChanged();
+            }
+        }
+        #endregion
+
+
+        #region IsHelloAnimationReady変更通知プロパティ
+        private bool _IsHelloAnimationReady = false;
+
+        public bool IsHelloAnimationReady
+        {
+            get
+            { return _IsHelloAnimationReady; }
+            set
+            {
+                _IsHelloAnimationReady = value;
+                RaisePropertyChanged();
+            }
+        }
+        #endregion
+
+
         #endregion
 
 
@@ -64,6 +130,7 @@ namespace ClientTest.ViewModels
             }
         }
 
+
         public bool CanLogin()
         {
             return true;
@@ -74,14 +141,42 @@ namespace ClientTest.ViewModels
             //ログイン処理
             try
             {
-                await _session.LoginAsync(UserName, Password);
+                //非同期でログイン
+                var loginTask = _session.LoginAsync(UserName, Password);
+                //メッセージを表示して待つ
+                IsMenuHidden = true;
+
+                //ここで待つ
+                await loginTask;
+
+                //成功したら閉じる
+                if (_session.IsLoggedIn)
+                {
+                    IsHelloAnimationReady = true;
+
+                    //ようこそメッセージを表示
+                    LoadMessage = "ようこそ " + _username + " さん！";
+
+                    // タイマーを作成する
+                    var timer = new DispatcherTimer();
+                    timer.Interval = TimeSpan.FromSeconds(1.5);
+                    timer.Tick += new EventHandler( (sender, e) => 
+                    {
+                        //タイマー内でウィンドウを閉じる
+                        Messenger.Raise(new WindowActionMessage(WindowAction.Close, "Close"));
+                    });
+                    // タイマーの実行開始
+                    timer.Start();
+                }
             }
             catch(ApplicationException ex)
             {
-                MessageBox.Show(ex.Message);
-            }
+                IsMenuHidden = false;
 
-            Messenger.Raise(new WindowActionMessage(WindowAction.Close,"Close"));
+                NotifyMessage = ex.Message;
+                //MessageBox.Show(ex.Message);
+            }
+            
         }
         #endregion
 
@@ -92,6 +187,9 @@ namespace ClientTest.ViewModels
 
         public void Initialize()
         {
+            IsMenuHidden = false;
+            LoadMessage = "ログインしています……";
+
         }
 
 #region IDataErrorInfo
