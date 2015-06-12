e1b82694396b6babf6735ceda83b013e34d4f2a0
doncot <doncot@gmail.com>
2015-06-12 15:09:30 +0900

    整理

--- ClientTest/Validator/UserNameValidator.cs
+++ ClientTest/Validator/UserNameValidator.cs
@@ -10,15 +10,10 @@ namespace ClientTest.Validator
 {
     class UserNameValidator : ValidationRule
     {
-        private static readonly Regex AlphaNumUnderscoreRegex = new Regex(@"^[a-zA-Z0-9_]+$", RegexOptions.Compiled);
+        private static readonly Regex AlphaNumUnderscoreRegex = new Regex(@"^[a-zA-Z0-9_]*$", RegexOptions.Compiled);
 
         public override ValidationResult Validate(object value, System.Globalization.CultureInfo cultureInfo)
         {
-            //if (value.ToString().Length < 1)
-            //{
-            //    return new ValidationResult(false, "Age field cannot be empty");
-            //}
-
             if (!AlphaNumUnderscoreRegex.IsMatch(value.ToString()))
             {
                 return new ValidationResult(false, "半角英数字とアンダースコア(_)のみ使用可能です");
