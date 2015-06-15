c9dabcf15f74c3aea53d9383f39ddf6d8ccc2bb1
doncot <doncot@gmail.com>
2015-06-16 04:20:23 +0900

    なし

---
+++ UserManageUtility/Converter/RoleNameConverter.cs
@@ -0,0 +1,19 @@
+using System.Globalization;
+using System.Windows.Data;
+
+namespace UserManageUtility.Converter
+{
+    public class RoleNameConverter : IValueConverter
+    {
+        public object Convert(object value, System.Type type, object obj, CultureInfo cl)
+        {
+            return null;
+        }
+
+        public object ConvertBack(object value, System.Type type, object obj, CultureInfo cl)
+        {
+            return null;
+        }
+        
+    }
+}
