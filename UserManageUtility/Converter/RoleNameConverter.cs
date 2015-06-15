cc71e75c96ea08fd2ca5122660b657c2d3c7291d
misono <misono@sos.info.hiroshima-cu.ac.jp>
2015-06-16 02:41:30 +0900

    表示を適正な

---
+++ UserManageUtility/Converter/RoleNameConverter.cs
@@ -0,0 +1,37 @@
+﻿using ClientTest.Models;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace UserManageUtility.Converter
+{
+    public class RoleNameConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value == null) return null;
+
+            var roles = value as ICollection<Role>;
+
+            if (roles.Count == 0) return null;
+
+            String resultString = "";
+            foreach(var r in roles)
+            {
+                resultString += String.IsNullOrEmpty(resultString) ? r.Name : ", " + r.Name;
+            }
+
+            return resultString;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
